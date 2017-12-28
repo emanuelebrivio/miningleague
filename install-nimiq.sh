c6cbfcdc20d0d177984796ee9058d296bad4ea18
Emanuele Brivio <emanuele.brivio@cloudacademy.com>
2017-12-28 10:35:25 +0100

    Cloning the release branch

--- install-nimiq.sh
+++ install-nimiq.sh
@@ -1,7 +1,7 @@
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get install -y git build-essential nodejs
 
-git clone https://github.com/nimiq-network/core.git
+git clone -b release https://github.com/nimiq-network/core.git
 cd core
 
 npm install
