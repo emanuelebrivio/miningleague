d8c11595dada4620720e220ef1a0a3b68c3f6595
Emanuele Brivio <emanuele.brivio@cloudacademy.com>
2017-12-28 10:31:03 +0100

    Better install script

--- install-nimiq.sh
+++ install-nimiq.sh
@@ -1,7 +1,5 @@
-sudo apt-get install -y git build-essential
-
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y git build-essential nodejs
 
 git clone https://github.com/nimiq-network/core.git
 cd core
